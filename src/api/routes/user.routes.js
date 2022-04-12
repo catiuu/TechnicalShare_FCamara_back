faa3ecc6c018ac56888e60c5a94424cf5e030c8c
ANNEBORTOLI <annebortoli@gmail.com>
2022-04-12 12:37:24 -0300

    :stethoscope:adição de middleware de checagem do user

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const auth = require("../middlewares/authToken");
+const checkUser = require("../middlewares/checkUser");
 
 // Faz login do Usuário e retorna um token
 router.post("/login", userController.login);
 // Rota de atualizar Perfil do usuário
-router.patch("/profile", userController.updateProfile);
+router.patch("/profile", auth, checkUser, userController.updateProfile);
 //Rota de adicionar Skill do usuário
-router.post("/addskills", userController.addSkill);
+router.post("/addskills", auth, checkUser, userController.addSkill);
 // Rota para deletar uma skill do usuário
-router.post("/removeskills", userController.removeSkill);
+router.post("/removeskills", auth, checkUser, userController.removeSkill);
 // Busca lista de usuários com base no parâmetro de busca
-router.post("/search", userController.findUsers);
+router.post("/search", auth, userController.findUsers);
 // Busca um usuário pelo id
-router.post("/id", userController.findUserByPk);
+router.post("/id", auth, userController.findUserByPk);
 
 module.exports = router;
